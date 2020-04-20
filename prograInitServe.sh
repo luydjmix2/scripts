079719cb6b7717523e989a17e1c4d955e824f906
luydjmix2 <luydjmix@hotmail.com>
2020-04-20 12:36:14 -0500

    commit 3
    actualizacion #3

--- prograInitServe.sh
+++ prograInitServe.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
 #actulisar servidor
+echo "Actulisar servidor"
 yum -y update &> /dev/null
 echo "10%"
 #instalar ifconfig
+echo "Instalar ifconfig"
 yum -y install net-tools &> /dev/null
 echo "12%"
 #instalar nano
+echo "Instalar nano"
 yum -y install nano &> /dev/null
 echo "13%"
-#insalar apache
+#instalar apache
+echo "Instalar apache"
 yum -y install httpd &> /dev/null
 echo "15%"
 systemctl start httpd
 systemctl enable httpd
 systemctl restart httpd
 #instalar php
+echo "Instalar php"
 yum -y install php php-mbstring php-pear &> /dev/null
 systemctl restart httpd
 echo "20%"
 #instalar phpMyAdmin y mariaDb
+echo "Instalar phpMyAdmin y mariaDb"
 yum -y install httpd mariadb-server mariadb php php-mysql php-gd php-pear php-mbstring &> /dev/null
 systemctl start mariadb
 systemctl enable mariadb
