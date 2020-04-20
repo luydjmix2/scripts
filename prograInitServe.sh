c9ce8a27b0f05286a29640ae0e1e0f6402092417
luydjmix2 <luydjmix@hotmail.com>
2020-04-20 12:30:36 -0500

    commen 2
    modificacion #2 del script

--- prograInitServe.sh
+++ prograInitServe.sh
@@ -12,3 +12,21 @@ echo "13%"
 #insalar apache
 yum -y install httpd &> /dev/null
 echo "15%"
+systemctl start httpd
+systemctl enable httpd
+systemctl restart httpd
+#instalar php
+yum -y install php php-mbstring php-pear &> /dev/null
+systemctl restart httpd
+echo "20%"
+#instalar phpMyAdmin y mariaDb
+yum -y install httpd mariadb-server mariadb php php-mysql php-gd php-pear php-mbstring &> /dev/null
+systemctl start mariadb
+systemctl enable mariadb
+firewall-cmd --zone=public --remove-port=80/tcp --permanent
+firewall-cmd --zone=public --remove-port=3306/tcp --permanent
+firewall-cmd --reload
+echo "50%"
+
+
+
