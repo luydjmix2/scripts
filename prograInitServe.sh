74951565b0c7cf3efdd0ce33d011621aa9986c51
luydjmix2 <luydjmix@hotmail.com>
2020-05-09 21:19:02 -0500

    Update prograInitServe.sh

--- prograInitServe.sh
+++ prograInitServe.sh
@@ -1,50 +1,7 @@
 #!/bin/bash
+ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
-#actulisar servidor
-echo "Actulisar servidor"
-yum -y update
-echo "10%"
-#instalar ifconfig
-echo "Instalar ifconfig"
-yum -y install net-tools
-echo "12%"
-#instalar nano
-echo "Instalar nano"
-yum -y install nano
-echo "13%"
-#instalar apache
-echo "Instalar apache"
-yum -y install httpd
-echo "15%"
-systemctl start httpd
-systemctl enable httpd
-systemctl restart httpd
-#instalar php
-echo "Instalar php"
-rpm -iUvh https://download-ib01.fedoraproject.org/pub/epel/6/SRPMS/Packages/e/epel-release-6-8.src.rpm
-yum install -y yum-utils
-yum-config-manager --enable remi-php74
-yum update -y
-yum -y install php php-mysqlnd php-pdo php-mbstring php-pear php-pecl-zip php-mcrypt php-gd php-gd php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-soap curl curl-develyum -y install php php-mysqlnd php-pdo php-mbstring php-pear php-pecl-zip php-mcrypt php-gd php-gd php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-soap curl curl-devel
-systemctl restart httpd
-echo "20%"
-#instalar Unzip
-echo "Intalando Unzip"
-yum install unzip
-Echo "21%"
-#instalar phpMyAdmin y mariaDb
-echo "Instalar phpMyAdmin y mariaDb"
-yum -y install mariadb-server mariadb
-systemctl restart httpd
-echo "30%"
-yum install phpmyadmin -y
-echo "50%"
-systemctl start mariadb
-systemctl enable mariadb
-firewall-cmd --zone=public --add-port=80/tcp --permanent
-firewall-cmd --zone=public --add-port=3306/tcp --permanent
-firewall-cmd --reload
-echo "52%"
+echo ip
 
 
 
