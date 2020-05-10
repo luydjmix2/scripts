8f9724d8d3c11d62b4b898d5e32977aafe407a2e
luydjmix2 <luydjmix@hotmail.com>
2020-05-09 21:20:40 -0500

    Update prograInitServe.sh

--- prograInitServe.sh
+++ prograInitServe.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
+$ip="$(ifconfig | grep -A 1 'eth0' | tail -1 | cut -d ':' -f 2 | cut -d ' ' -f 1)"
 
-echo ip
+echo $ip
 
 
 
