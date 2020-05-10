504caf83e5d71ae2c8f860e555883047fcd9a52d
luydjmix2 <luydjmix@hotmail.com>
2020-05-10 14:47:19 -0500

    Commit
    Actualizacion 02

--- prograInitServe_Centos7i386.sh
+++ prograInitServe_Centos7i386.sh
@@ -28,5 +28,14 @@ sleep 2
 echo "Intalar epel wget screen"
 sleep 2
 yum -y install epel-release wget screen
+subscription-manager repos --enable rhel-7-server-optional-rpms --enable rhel-7-server-extras-rpms
 echo "16%"
 sleep 2
+#instalar mariaDb php phpmyadmin
+echo "Instalar MariaDb Php PhpMyAdmin"
+sleep 2
+yum -y install mariadb-server mariadb php74-php php74-php-mysql php74-php-gd php74-php-pear php74-php-mbstring
+yum repolist
+yum check-update
+yum -y install phpmyadmin
+
